c02c9a580e1520315180029c633f25e0cbf696a0
faizanmf-SDET <faizanmf.1999@gmail.com>
2026-04-08 00:14:54 +0530

    Update Program01.java

--- src/test/java/GitCommands/Program01.java
+++ src/test/java/GitCommands/Program01.java
@@ -3,6 +3,7 @@ package GitCommands;
 public class Program01 {
     public static void main(String[] args) {
         System.out.println("Hello Faizan!");
+        System.out.println("Hello world! - pull 1 file");
 
     }
 }
